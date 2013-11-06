6f8fb50cdaf8a3fb22456b04770bfd3d6059a1ab
Andrei Nicholson <contact@andreinicholson.com>
2013-11-05 19:30:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+Piwik Tracking Plugin for Roundcube
+===================================
+
+This plugin integrates Piwik analytics using the
+[Piwik Tracking API](http://piwik.org/docs/tracking-api/) into Roundcube. This
+plugin is very different from the
+[Roundcube Webmail piwik analytics plugin](http://blog.no-panic.at/projects/roundcube-webmail-piwik-analytics-plugin/)
+which adds the client-side
+[JavaScript Tracking Tag](http://piwik.org/docs/javascript-tracking/). The aim
+of this plugin is to add Piwik integration on the server-side in order to get
+around same-origin policy.
+
+If your Piwik installation is on the same domain as your Roundcube
+installation and both are using the same protocol, then this plugin is
+probably not what you need as it will not add any significant value over the
+JavaScript Tracking Tag. Some example cases where this plugin shines:
+
+* Piwik at `http://analytics.company.com`, Roundcube at
+  `https://webmail.company.com`
+* Piwik at `http://analytics.company.com`, Roundcube at
+  `http://webmail.othercompany.com`
+
+The version numbers used reflect three different components: supported
+Roundcube version, supported Piwik Tracking API library, version of the this
+pllugin for the two Rouncube and Piwik components. For example, the initial
+version of this plugin which supports Roundcube 0.9.5 and Piwik Tracking API
+1 is rc0.9.5-piwik1-1.0. The compatibility list for the Piwik Tracking API
+against versions of Piwik is unknown, so go with the highest available
+version if your Piwik installation is kept reasonably up to date.
+
+## Install
+
+Install using Composer or manually download and install into
+`plugins/piwik_tracking_api`. Copy `config.inc.php.dist` to `config.inc.php`
+in the same directory and edit the file using the options shown below.
+
+Add piwik_tracking_api to `$config['plugins']` in your Roundcube config to
+enable the plugin.
+
+## Configuration
+
+Copy `config.inc.php.dist` to `config.inc.php` and edit the configuration
+variables.
+
+**piwik_tracking_api_url** [string]
+
+Set this to the URL of the Piwik installation. This URL must be accessible
+from the Roundcube installation.
+
+**piwik_tracking_api_site_id** [int|array(string => int)]
+
+Configures the Piwik site ID. The value of this variable can either be a
+single integer or an array containing multiple server names and IDs in cases
+where a single Roundcube installation serves multiple hosts.
+
+To set multiple hosts, use the key/value pair of server name and Piwik website
+ID. For example:
+
+```php
+$rcmail_config['piwik_tracking_api_site_id'] = array(
+    'webmail.foo.com' => 42,
+    'webmail.bar.com' => 13
+);
+```
+
+**piwik_tracking_api_token_auth** [string] _(Optional)_
+
+Set to the token auth key of a Piwik user in order to take advantage of
+advanced tracking. Currently utilizes the following if provided:
+
+* Sets remote IP to that of the user instead of defaulting to the IP of the
+  Roundcube installation.
---
+++ piwik_tracking_api.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Piwki Tracking API.
+ *
+ * Adds the PHP Piwki tracking API.
+ *
+ * @version rc0.9.5-piwik1-1.0
+ * @author  Andrei Nicholson
+ * @url     https://github.com/tetsuo13/Roundcube-Piwik-Tracking-Api-Plugin
+ */
+class piwik_tracking_api extends rcube_plugin
+{
+    /**
+     * Entry point for plugin. Track on all events.
+     */
+    public function init()
+    {
+        require_once dirname(__FILE__) . '/PiwikTracker.php';
+
+        $rcmail = rcmail::get_instance();
+
+        $this->load_config();
+
+        $trackingUrl = $this->getTrackingUrl($rcmail);
+
+        if ($trackingUrl === false) {
+            return;
+        }
+
+        PiwikTracker::$URL = $trackingUrl;
+
+        $siteId = $this->getSiteId($rcmail);
+
+        if ($siteId === false) {
+            return;
+        }
+
+        $tracker = new PiwikTracker($siteId);
+
+        $tokenAuth = $rcmail->config->get('piwik_tracking_api_token_auth', null);
+
+        if ($tokenAuth !== null) {
+            $tracker->setTokenAuth($tokenAuth);
+        }
+
+        if ($this->gset('HTTP_USER_AGENT')) {
+            $tracker->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+        }
+
+        $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on' ? 'https://' : 'http://')
+             . $_SERVER['SERVER_NAME']
+             . $_SERVER['REQUEST_URI']);
+
+        $tracker->setUrl();
+
+        if ($this->gset('HTTP_REFERER')) {
+            $tracker->setUrlReferer($_SERVER['HTTP_REFERER']);
+        }
+
+        if ($tokenAuth !== null && $this->gset('REMOTE_ADDR')) {
+            $tracker->setIp($_SERVER['REMOTE_ADDR']);
+        }
+
+        $tracker->doTrackPageView();
+    }
+
+    /**
+     * Check for superglobal _SERVER key.
+     *
+     * @param string $i Key to search for.
+     * @return bool True if it exists.
+     */
+    private function gset($i)
+    {
+        return isset($_SERVER[$i]);
+    }
+
+    /**
+     * Get the required Piwik tracking URL from config.
+     *
+     * @param rcube $rcmail Roundcube object.
+     * @return string Tracking URL.
+     */
+    private function getTrackingUrl(rcube $rcmail)
+    {
+        $trackingUrl = $rcmail->config->get('piwik_tracking_api_url', null);
+
+        if ($trackingUrl === null) {
+            rcmail::raise_error(
+                array(
+                    'code' => 2,
+                    'type' => 'php',
+                    'file' => __FILE__,
+                    'line' => __LINE__,
+                    'message' => 'tracking URL is required for the piwik_tracking_api plugin'
+                ),
+                true,
+                false
+            );
+            return false;
+        }
+
+        return $trackingUrl;
+    }
+
+    /**
+     * Get the required Piwik site ID from config.
+     *
+     * @param rcmail $rcmail Roundcube object.
+     * @return int Site ID.
+     */
+    private function getSiteId(rcmail $rcmail)
+    {
+        $siteId = $rcmail->config->get('piwik_tracking_api_site_id', null);
+
+        if ($siteId === null) {
+            rcmail::raise_error(
+                array(
+                    'code' => 3,
+                    'type' => 'php',
+                    'file' => __FILE__,
+                    'line' => __LINE__,
+                    'message' => 'site ID required for the piwik_tracking_api plugin'
+                ),
+                true,
+                false
+            );
+            return false;
+        }
+
+        if (!is_array($siteId)) {
+            return $siteId;
+        }
+
+        if (isset($siteId[$_SERVER['SERVER_NAME']])) {
+            return $siteId[$_SERVER['SERVER_NAME']];
+        }
+
+        rcmail::raise_error(
+            array(
+                'code' => 4,
+                'type' => 'php',
+                'file' => __FILE__,
+                'line' => __LINE__,
+                'message' => 'unable to find ' . $_SERVER['SERVER_NAME'] . ' in site ID array for piwik_tracking_api plugin'
+            ),
+            true,
+            false
+        );
+
+        return false;
+    }
+}
+
